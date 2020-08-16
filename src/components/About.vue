5727719ea07cf5942b052a746f947ec21940674d
psyked <psyked@DESKTOP-3AGL3NA.localdomain>
2020-08-16 10:18:50 +0200

    fix Color on About.social.rawIcon and fix Skills.vue's identation

--- src/components/About.vue
+++ src/components/About.vue
@@ -24,9 +24,7 @@
             :alt="`${$t('about.iconOf')} ${social.name}`"
           >
             <font-awesome-icon :icon="['fab', social.icon]" v-if="social.icon"></font-awesome-icon>
-            <svg v-if="social.rawSVG" class="mb-1 svg-inline--fa fa-w-20">
-              <path :d="social.rawSVG" />
-            </svg>
+            <svg v-if="social.rawSVG" v-html="social.rawSVG" class="mb-1 svg-inline--fa fa-w-20" />
           </a>
         </div>
         <b-button
@@ -121,8 +119,14 @@ export default {
         {
           name: "BuyMeACoffee",
           url: "https://www.buymeacoffee.com/psyked",
-          rawSVG:
-            "M9.197 0l-1.619 3.735h-2.407v3.359h0.921l0.943 5.975h-1.473l1.948 10.973 1.249-0.015 1.256 7.973h11.891l0.083-0.531 1.172-7.443 1.188 0.015 1.943-10.973h-1.407l0.937-5.975h1.011v-3.359h-2.557l-1.625-3.735zM9.901 1.073h12.057l1.025 2.375h-14.115zM6.235 4.803h19.525v1.228h-19.525zM6.839 14.136h18.183l-1.568 8.823-7.536-0.079-7.511 0.079z",
+          rawSVG: `
+            <path fill="currentColor" d="
+              M9.197 0l-1.619 3.735h-2.407v3.359h0.921l0.943 5.975h-1.473l1.948 10.973 1.249-0.015 1.256 7.973h11.891l0.083-0.531 1.172-7.443 1.188 0.015 1.943-10.973h-1.407l0.937-5.975h1.011v-3.359h-2.557l-1.625-3.735z
+              M9.901 1.073h12.057l1.025 2.375h-14.115z
+              M6.235 4.803h19.525v1.228h-19.525z
+              M6.839 14.136h18.183l-1.568 8.823-7.536-0.079-7.511 0.079z"
+            />
+          `,
         },
       ],
     };
