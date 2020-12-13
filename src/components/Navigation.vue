96fc4e1ebdbd538f48c543fba4b4146093f2f37f
psyked <psyked@DESKTOP-3AGL3NA.localdomain>
2020-12-13 22:25:51 +0100

    fix #10 : fixed margin for language buttons

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,7 +1,14 @@
 <template>
-  <b-navbar toggleable="lg" type="dark" class="bg-primary fixed-top" id="sideNav">
+  <b-navbar
+    toggleable="lg"
+    type="dark"
+    class="bg-primary fixed-top"
+    id="sideNav"
+  >
     <b-navbar-brand to="#app">
-      <span class="d-block d-lg-none">{{ $t('name.first') }} {{ $t('name.last') }}</span>
+      <span class="d-block d-lg-none"
+        >{{ $t("name.first") }} {{ $t("name.last") }}</span
+      >
       <span class="d-none d-lg-block">
         <img
           class="img-fluid img-profile rounded-circle mx-auto mb-2"
@@ -20,8 +27,9 @@
               v-for="(nav, index) in $t('navigation.listNav')"
               :key="index"
               class="js-scroll-trigger"
-              :href="'#'+index"
-            >{{ nav }}</b-nav-item>
+              :href="'#' + index"
+              >{{ nav }}</b-nav-item
+            >
           </div>
           <hr class="d-lg-block d-none invisible" />
           <div>
@@ -31,7 +39,10 @@
                 v-on:click="toggleTheme()"
                 :title="$t('navigation.changeTheme')"
               >
-                <font-awesome-icon size="2x" :icon="[themeMode==='light'?'fas':'far', 'lightbulb']"></font-awesome-icon>
+                <font-awesome-icon
+                  size="2x"
+                  :icon="[themeMode === 'light' ? 'fas' : 'far', 'lightbulb']"
+                ></font-awesome-icon>
               </b-button>
             </b-nav-item>
             <b-nav-item>
@@ -40,7 +51,10 @@
                 v-b-modal.modal-languages
                 :title="$t('navigation.language')"
               >
-                <font-awesome-icon icon="language" size="2x"></font-awesome-icon>
+                <font-awesome-icon
+                  icon="language"
+                  size="2x"
+                ></font-awesome-icon>
               </b-button>
             </b-nav-item>
             <b-modal
@@ -56,26 +70,29 @@
               return-focus="null"
               size="sm"
             >
-              <div class="text-center">
-                <b-button
-                  class="col-sm-12 mb-1 btn-language"
-                  v-for="(lang, id) in languages"
-                  :key="id"
-                  @click="changeLocale(lang.language)"
-                  :variant="outlineColor"
-                  :pressed="isLanguageSelected(lang.language)"
-                  :lang="lang.language"
-                  squared
-                >
-                  <flag :iso="lang.flag" v-bind:squared="false" />
-                  {{lang.name}}
-                  <font-awesome-icon
-                    :icon="['fas', 'check']"
-                    v-if="isLanguageSelected(lang.language)"
-                  ></font-awesome-icon>
-                  <label v-else></label>
-                </b-button>
-              </div>
+              <b-button
+                class="col-sm-12 mb-1 btn-language"
+                v-for="(lang, id) in languages"
+                :key="id"
+                @click="changeLocale(lang.language)"
+                :variant="outlineColor"
+                :pressed="isLanguageSelected(lang.language)"
+                :lang="lang.language"
+                squared
+              >
+                <flag
+                  class="w-25"
+                  :iso="lang.flag"
+                  v-bind:squared="false"
+                />
+                <label class="w-50">{{ lang.name }}</label>
+                <font-awesome-icon
+                  class="w-25 mt-2"
+                  :icon="['fas', 'check']"
+                  v-if="isLanguageSelected(lang.language)"
+                ></font-awesome-icon>
+                <label class="w-25" v-else></label>
+              </b-button>
             </b-modal>
           </div>
         </div>
